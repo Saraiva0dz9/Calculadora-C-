660ce711ccc1d709250f16c0c3216cb6ca50a8db
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2022-11-10 16:34:23 -0300

    Iniciando projeto

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿using System
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse( Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
