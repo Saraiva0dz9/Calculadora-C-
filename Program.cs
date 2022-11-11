bc10b5da31bb9fcd8f54f6bcae1dd2469a143851
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2022-11-10 21:17:34 -0300

    Function: soma

--- Program.cs
+++ Program.cs
@@ -6,11 +6,21 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma(){
             Console.Clear();
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse( Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+             
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado do soma é " + resultado );
         }
     }
 }
